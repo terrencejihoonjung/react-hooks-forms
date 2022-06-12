a2bd7d2776870ffb4335eaa5161e620029f4ea33
--replace-all <terrencejung928@gmail.com>
2022-06-11 17:15:36 -0700

    commit tree 32cfb0b313944b9b34e56d9d2468270374961737

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form({
+  firstName, 
+  lastName, 
+  handleFirstNameChange, 
+  handleLastNameChange
+}) {
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstNameChange}/>
+      <input type="text" value={lastName} onChange={handleLastNameChange}/>
       <button type="submit">Submit</button>
     </form>
   );
